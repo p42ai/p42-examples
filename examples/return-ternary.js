c857ccfd8fc970bcffa2a1e4626c9ce1d948499c
Lars Grammel <lars.grammel@gmail.com>
2021-07-01 10:45:17 +0200

    Enhanced return-ternary test.

--- examples/return-ternary.js
+++ examples/return-ternary.js
@@ -1,7 +1,25 @@
-const f = () => {
+const f1 = () => {
   console.log("test");
   if (a) {
     return x;
   }
   return y;
+};
+
+const f2 = () => {
+  console.log("test");
+  if (a) {
+    return x;
+  } else {
+    return y;
+  }
+};
+
+const f3 = () => {
+  console.log("test");
+  if (a) {
+    return x;
+  }
+
+  return b ? y : z;
 };
