cfc3df1d46e704c68aa8a7ff78f9b5e759d69b79
Lars Grammel <lars.grammel@gmail.com>
2021-07-31 18:43:55 +0200

    Updates.

--- examples/string-ends-with.ts
+++ examples/string-ends-with.ts
@@ -1,2 +1,3 @@
 let s: string = "123";
 const match = s[s.length - 1] != "/";
+const match2 = s.charAt(s.length - 1) == "/";
---
+++ examples/string-starts-with.ts
@@ -0,0 +1,3 @@
+let s: string = "123";
+const match = s[0] != "/";
+const match2 = s.charAt(0) == "/";
